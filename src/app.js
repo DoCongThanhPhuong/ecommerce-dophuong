0b56f891a8d38c9ca0d1c9ef1eeb0845a38bceca
DoCongThanhPhuong <bido23082003@gmail.com>
2024-05-09 07:19:27 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const { default: helmet } = require('helmet')
+const compression = require('compression')
+const app = express()
+
+// init middlewares
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
+
+// init database
+require('./dbs/init.mongodb')
+const { checkOverload } = require('./helpers/check.connect')
+checkOverload()
+
+// init routes
+app.use('/', require('./routes'))
+
+// handling error
+
+module.exports = app
